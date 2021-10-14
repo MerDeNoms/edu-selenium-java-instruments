dc558c286ee4c1fe3f9e1b092ebd263d32acebc2
MerDeNoms <bjorn.alex@yandex.ru>
2021-10-14 21:27:15 +0300

    Page Object is finished

--- src/main/java/SignUpPage.java
+++ src/main/java/SignUpPage.java
@@ -1,26 +1,97 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class SignUpPage  {
-    WebDriver driver;
+    private WebDriver driver;
 
     public SignUpPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    By emailField = By.cssSelector("input#email");
-    By confirmField = By.cssSelector("input#confirm");
-    By passwordField = By.cssSelector("input#password");
-    By nameField = By.cssSelector("input#displayname");
-    By dayField = By.cssSelector("input#day");
-    By monthDropDown = By.cssSelector("select#month");
-    String monthDropDownOption = "//select[@id='month']/option[text()='%s']";
-    By yearField = By.cssSelector("input#year");
-    String sexRadioButton = "//span[text()='%s']//..//..";
-    By shareCheckbox = By.xpath("//span[text()='Я разрешаю сообщить мои регистрационные данные партнерам Spotify в целях рекламы.']");
-    By registerButton = By.xpath("//button[@type='submit']");
-    By errorLabel = By.xpath("//div[@aria-label = 'Значок ошибки']");
-    String errorByText = "//div[@aria-label = 'Значок ошибки' and text() = '%s']";
+    private By emailField = By.cssSelector("input#email");
+    private By confirmField = By.cssSelector("input#confirm");
+    private By passwordField = By.cssSelector("input#password");
+    private By nameField = By.cssSelector("input#displayname");
+    private By dayField = By.cssSelector("input#day");
+    private By monthDropDown = By.cssSelector("select#month");
+    private String monthDropDownOption = "//select[@id='month']/option[text()='%s']";
+    private By yearField = By.cssSelector("input#year");
+    private String sexRadioButton = "//span[text()='%s']//..//..";
+    private By shareCheckbox = By.xpath("//span[text()='Я разрешаю сообщить мои регистрационные данные партнерам Spotify в целях рекламы.']");
+    private By registerButton = By.xpath("//button[@type='submit']");
+    private By errorLabel = By.xpath("//div[@aria-label = 'Значок ошибки']");
+    private String errorByText = "//div[@aria-label = 'Значок ошибки' and text() = '%s']";
 
+    public SignUpPage typeEmail(String email) {
+        driver.findElement(emailField).sendKeys(email);
+        return this;
+    }
+
+    public SignUpPage typeConfirmEmail(String email) {
+        driver.findElement(confirmField).sendKeys(email);
+        return this;
+    }
+
+    public SignUpPage typePassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+        return this;
+    }
+
+    public SignUpPage typeName(String name) {
+        driver.findElement(nameField).sendKeys(name);
+        return this;
+    }
+
+    public SignUpPage typeDay(String day) {
+        driver.findElement(dayField).sendKeys(day);
+        return this;
+    }
+
+    public SignUpPage setMonth(String month) {
+        driver.findElement(monthDropDown).click();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(String.format(monthDropDownOption, month)))).click();
+        return this;
+    }
+
+    public SignUpPage typeYear(String year) {
+        driver.findElement(yearField).sendKeys(year);
+        return this;
+    }
+
+    public SignUpPage setSex(String value) {
+        driver.findElement(By.xpath(String.format(sexRadioButton, value))).click();
+        return this;
+    }
+
+    public SignUpPage setShare(boolean value) {
+        WebElement checkbox = driver.findElement(shareCheckbox);
+        if (!checkbox.isSelected() == value) {
+            checkbox.click();
+        }
+        return this;
+    }
+
+    public void clickSignUpButton() {
+        driver.findElement(registerButton).click();
+    }
+
+    public List<WebElement> getErrors() {
+        return driver.findElements(errorLabel);
+    }
+
+    public String getErrorByNumber(int number) {
+        return getErrors().get(number - 1).getText();
+    }
+
+    public boolean isErrorVisible(String message) {
+        return driver.findElements(By.xpath(String.format(errorByText, message))).size() > 0
+                && driver.findElements(By.xpath(String.format(errorByText, message))).get(0).isDisplayed();
+    }
 
 }
